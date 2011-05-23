6cc1a0db6970cdf0f87a69e4d620e74bccfb620d
ersthelfer <ersthelfer>
2011-05-23 12:11:49 +0000

    getRelativePath hinzu
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@128 5890f383-b09f-485f-8f59-22bc967d67fd

--- src/de/droidcachebox/main.java
+++ src/de/droidcachebox/main.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.Timer;
 import java.util.TimerTask;
 import de.droidcachebox.Components.ActivityUtils;
@@ -171,6 +172,10 @@ public class main extends Activity implements SelectedCacheEvent,LocationListene
     	if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK){
                 Log.d("DroidCachebox","Picture taken!!!");
+                Global.selectedCache.ReloadSpoilerRessources();
+                String MediaFolder = Config.GetString("UserImageFolder");
+            	String TrackFolder = Config.GetString("TrackFolder");
+            	String relativPath = getRelativePath(MediaFolder, TrackFolder, "\\"); 
                 return;
             } else
             {
@@ -885,5 +890,54 @@ public class main extends Activity implements SelectedCacheEvent,LocationListene
         return sDate;
     }
 
+    public static String getRelativePath(String targetPath, String basePath, String pathSeparator) 
+    {   //  We need the -1 argument to split to make sure we get a trailing      
+    	//  "" token if the base ends in the path separator and is therefore     
+    	//  a directory. We require directory paths to end in the path     
+    	//  separator -- otherwise they are indistinguishable from files.     
+    	String[] base = basePath.split(Pattern.quote(pathSeparator), -1);     
+    	String[] target = targetPath.split(Pattern.quote(pathSeparator), 0);     
+    	//  First get all the common elements. Store them as a string,    
+    	//  and also count how many of them there are.      
+    	String common = "";     
+    	int commonIndex = 0;     
+    	for (int i = 0; i < target.length && i < base.length; i++) 
+    	{         
+    		if (target[i].equals(base[i])) 
+    		{             
+    			common += target[i] + pathSeparator;             
+    			commonIndex++;         
+    		}         
+    		else break;     
+    	}     
+
+        if (commonIndex == 0)     
+        {         //  Whoops -- not even a single common path element. This most         
+        	//  likely indicates differing drive letters, like C: and D:.          
+        	//  These paths cannot be relativized. Return the target path.         
+        	return targetPath;         
+        	//  This should never happen when all absolute paths
+        	//  begin with / as in *nix.      
+        }     
+        String relative = "";     
+        if (base.length == commonIndex) 
+        {         
+        	//  Comment this out if you prefer that a relative path not start with ./         
+        	//relative = "." + pathSeparator;     
+        }     
+        else 
+        {         
+        	int numDirsUp = base.length - commonIndex - 1;        
+        	//  The number of directories we have to backtrack is the length of         
+        	//  the base path MINUS the number of common path elements, minus         
+        	//  one because the last element in the path isn't a directory.         
+        	for (int i = 1; i <= (numDirsUp); i++) 
+        	{             
+        		relative += ".." + pathSeparator;         
+        	}     
+        }     
+        relative += targetPath.substring(common.length());     
+        return relative; 
+    }
 	
 }
