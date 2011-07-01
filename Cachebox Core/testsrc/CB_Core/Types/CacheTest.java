15c2691deb1566c49021c88c8b26da74eaee5662
saarfuchs-de <saarfuchs-de>
2011-07-01 14:47:15 +0000

    JUnit - Cache-Entfernung getestet...
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@277 5890f383-b09f-485f-8f59-22bc967d67fd

--- Cachebox Core/testsrc/CB_Core/Types/CacheTest.java
+++ Cachebox Core/testsrc/CB_Core/Types/CacheTest.java
@@ -3,8 +3,43 @@ package CB_Core.Types;
 import junit.framework.TestCase;
 
 public class CacheTest extends TestCase {
+
+	private Cache mCache;
+
+	
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+		mCache = new Cache();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+		mCache = null;
+	}
+
 	public void testConstructor() {
-		Cache cache = new Cache();
-		assertTrue( "Objekt muss konstruierbar sein", cache!=null );
+		assertTrue( "Objekt muss konstruierbar sein", mCache!=null );
+	}
+	
+	public void testDistance() {
+		Coordinate coordinate1 = new Coordinate();
+		assertTrue( "Objekt muss konstruierbar sein", coordinate1!=null );
+		coordinate1.Latitude = 49.428333;
+		coordinate1.Longitude = 6.203333;
+		// Vorsicht - hier wird nur die Objekt-Referenz gesetzt...
+		mCache.Pos = coordinate1;
+
+		Coordinate coordinate2 = new Coordinate();
+		assertTrue( "Objekt muss konstruierbar sein", coordinate2!=null );
+		coordinate2.Latitude = 49.427700;
+		coordinate2.Longitude = 6.204300;
+		
+		float distance = mCache.Distance(coordinate2);
+		// assertTrue( "Entfernung muss 100m sein", distance==99.384 );
+		
 	}
 }
