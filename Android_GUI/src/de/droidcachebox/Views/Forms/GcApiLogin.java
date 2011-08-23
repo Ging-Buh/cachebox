b7ece5a30932233483b0aa73edbb609d962f8c30
Hubert Merkle <hubert@fmby.de>
2011-08-23 04:23:58 +0000

    Übergabe des AccessTokens in die Settings funktionierte noch nicht!
    Achtung!!!! Bitte noch nicht verwenden!!!!
    
    Attention!!! Please still do not use!!!!
    
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@388 5890f383-b09f-485f-8f59-22bc967d67fd

--- Android_GUI/src/de/droidcachebox/Views/Forms/GcApiLogin.java
+++ Android_GUI/src/de/droidcachebox/Views/Forms/GcApiLogin.java
@@ -1,44 +1,24 @@
 package de.droidcachebox.Views.Forms;
 
-import de.droidcachebox.Global;
 import de.droidcachebox.R;
-import de.droidcachebox.main;
 import CB_Core.Config;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.view.ViewParent;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 
 public class GcApiLogin extends Activity {
-//	private static final String TEAM_CACHEBOX_ASP_NET_URL = "http://aspspider.info/TeamCachebox/";
-//	private static final String TEAM_CACHEBOX_ASP_NET_URL = "http://geopt.sytes.net/Team-Cachebox/default.aspx"; 
-	private static final String TEAM_CACHEBOX_ASP_NET_URL = "http://aspspider.info/Longri/default.aspx"; 
 	private static GcApiLogin gcApiLogin;
-	private static ProgressDialog pd;
-	private static boolean pdIsShow=false;
-		
+
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gcapilogin);
 		gcApiLogin = this;
 
-		View titleView = getWindow().findViewById(android.R.id.title);
-		if (titleView != null) {
-		  ViewParent parent = titleView.getParent();
-		  if (parent != null && (parent instanceof View)) {
-		    View parentView = (View)parent;
-		    parentView.setBackgroundColor(Global.getColor(R.attr.TitleBarBackColor));
-		  }
-		}
-
-		
 		final WebView webView = (WebView) this.findViewById(R.id.gal_WebView);
 
 		webView.setWebViewClient(new WebViewClient() {
@@ -59,37 +39,7 @@ public class GcApiLogin extends Activity {
 					super.onPageFinished(view, url);
 			}
 			
-			
 		});
-		
-		
-		webView.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress)   
-            {
-             //Make the bar disappear after URL is loaded, and changes string to Loading...
-            	gcApiLogin.setTitle("Loading...");
-            	gcApiLogin.setProgress(progress); //Make the bar disappear after URL is loaded
-
-            	if(!pdIsShow)
-            	{
-            		pd = ProgressDialog.show(gcApiLogin, "", 
-		                 "Loading....", true);
-            		pdIsShow=true;
-            	}
-            	
-            	
-             // Return the app name after finish loading
-                if(progress == 100)
-                {
-                	gcApiLogin.setTitle(R.string.app_name);
-                	pd.dismiss();
-                	pdIsShow=false;
-                }
-                	
-              }
-            });
-
-		
 		WebSettings settings = webView.getSettings();
 
 		settings.setPluginsEnabled(true);
@@ -99,15 +49,14 @@ public class GcApiLogin extends Activity {
 		// webView.setWebChromeClient(new WebChromeClient());
 		webView.getSettings().setJavaScriptEnabled(true);
 
-		webView.loadUrl(TEAM_CACHEBOX_ASP_NET_URL);
+		webView.loadUrl("http://aspspider.info/TeamCachebox/");
 	}
 
-	class MyJavaScriptInterface 
-	{
-		public void showHTML(String html) 
-		{
-			//String search = "Authorized!  Access token: "; Longri change: bei mir gab es bei der Antwort nur ein Leerzeichen!
-			String search = "Authorized! Access token: ";
+	class MyJavaScriptInterface {
+
+		public void showHTML(String html) {
+
+			String search = "Authorized!  Access token: ";
 			int pos = html.indexOf(search);
 			if (pos < 0)
 				return;
@@ -117,15 +66,8 @@ public class GcApiLogin extends Activity {
 			// zwischen pos und pos2 sollte ein gültiges AccessToken sein!!!
 			String accessToken = html.substring(pos + search.length(), pos2);
 			Config.Set("GcAPI", accessToken);
-			Config.AcceptChanges();
-//			MessageBox.Show("AccessToken erfolgreich erstellt! " +accessToken);
-			if(Settings.Me!=null)
-			{
-				Settings.Me.setGcApiKey(accessToken);
-			}
+			MessageBox.Show("AccessToken erfolgreich erstellt!");
 			gcApiLogin.finish();
 		}
 	}
-	
-		
 }
