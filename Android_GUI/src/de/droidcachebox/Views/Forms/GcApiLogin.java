74f6d98bbe3fb1936ff1c168bdfd29bff8e97c3b
Hubert Merkle <hubert@fmby.de>
2011-08-24 21:02:29 +0000

    Teilweiser Revert: Create AccessToken: Bei mir wurden die AccessToken nicht mehr in die Settings eingetragen
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@393 5890f383-b09f-485f-8f59-22bc967d67fd

--- Android_GUI/src/de/droidcachebox/Views/Forms/GcApiLogin.java
+++ Android_GUI/src/de/droidcachebox/Views/Forms/GcApiLogin.java
@@ -2,32 +2,27 @@ package de.droidcachebox.Views.Forms;
 
 import de.droidcachebox.Global;
 import de.droidcachebox.R;
-import de.droidcachebox.main;
 import CB_Core.Config;
 import CB_Core.Api.CB_Api;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewParent;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 
 public class GcApiLogin extends Activity {
-
 	private static GcApiLogin gcApiLogin;
-	private static ProgressDialog pd;
-	private static boolean pdIsShow=false;
-		
+
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gcapilogin);
 		gcApiLogin = this;
-		
+
 		String GC_AuthUrl;
 		
 		if(Config.GetString("OverrideUrl").equals(""))
@@ -42,8 +37,8 @@ public class GcApiLogin extends Activity {
 		if(GC_AuthUrl.equals(""))
 		{
 			finish();
-		}
-
+		}		
+		
 		View titleView = getWindow().findViewById(android.R.id.title);
 		if (titleView != null) {
 		  ViewParent parent = titleView.getParent();
@@ -53,7 +48,6 @@ public class GcApiLogin extends Activity {
 		  }
 		}
 
-		
 		final WebView webView = (WebView) this.findViewById(R.id.gal_WebView);
 
 		webView.setWebViewClient(new WebViewClient() {
@@ -74,37 +68,7 @@ public class GcApiLogin extends Activity {
 					super.onPageFinished(view, url);
 			}
 			
-			
 		});
-		
-		
-		webView.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress)   
-            {
-             //Make the bar disappear after URL is loaded, and changes string to Loading...
-            	gcApiLogin.setTitle("Loading...");
-            	gcApiLogin.setProgress(progress); //Make the bar disappear after URL is loaded
-
-            	if(!pdIsShow)
-            	{
-            		pd = ProgressDialog.show(gcApiLogin, "", 
-		                 "Loading....", true);
-            		pdIsShow=true;
-            	}
-            	
-            	
-             // Return the app name after finish loading
-                if(progress == 100)
-                {
-                	gcApiLogin.setTitle(R.string.app_name);
-                	pd.dismiss();
-                	pdIsShow=false;
-                }
-                	
-              }
-            });
-
-		
 		WebSettings settings = webView.getSettings();
 
 		settings.setPluginsEnabled(true);
@@ -117,12 +81,11 @@ public class GcApiLogin extends Activity {
 		webView.loadUrl(GC_AuthUrl);
 	}
 
-	class MyJavaScriptInterface 
-	{
-		public void showHTML(String html) 
-		{
-			//String search = "Authorized!  Access token: "; Longri change: bei mir gab es bei der Antwort nur ein Leerzeichen!
-			String search = "Authorized! Access token: ";
+	class MyJavaScriptInterface {
+
+		public void showHTML(String html) {
+
+			String search = "Access token: ";
 			int pos = html.indexOf(search);
 			if (pos < 0)
 				return;
@@ -132,8 +95,6 @@ public class GcApiLogin extends Activity {
 			// zwischen pos und pos2 sollte ein gültiges AccessToken sein!!!
 			String accessToken = html.substring(pos + search.length(), pos2);
 			Config.Set("GcAPI", accessToken);
-			Config.AcceptChanges();
-//			MessageBox.Show("AccessToken erfolgreich erstellt! " +accessToken);
 			if(Settings.Me!=null)
 			{
 				Settings.Me.setGcApiKey(accessToken);
@@ -141,5 +102,4 @@ public class GcApiLogin extends Activity {
 			gcApiLogin.finish();
 		}
 	}
-	
 }
