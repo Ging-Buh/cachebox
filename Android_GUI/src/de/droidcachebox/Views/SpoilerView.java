c7d6a022800885a0e05b609b177fe826a4966286
longri <www.longri.de@googlemail.com>
2011-09-16 11:23:47 +0000

    + Fixed an out of memory exception in spoiler view, with load many big image 	System.gc() cleanup the memory before load another image
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@428 5890f383-b09f-485f-8f59-22bc967d67fd

--- Android_GUI/src/de/droidcachebox/Views/SpoilerView.java
+++ Android_GUI/src/de/droidcachebox/Views/SpoilerView.java
@@ -1,5 +1,6 @@
 package de.droidcachebox.Views;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import CB_Core.FileIO;
@@ -19,6 +20,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.os.AsyncTask;
@@ -35,7 +37,9 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class SpoilerView extends FrameLayout  implements ViewOptionsMenu, SelectedCacheEvent, AdapterView.OnItemSelectedListener{
+public class SpoilerView extends FrameLayout implements ViewOptionsMenu,
+		SelectedCacheEvent, AdapterView.OnItemSelectedListener
+{
 
 	RelativeLayout spoilerLayout;
 	Context context;
@@ -45,146 +49,163 @@ public class SpoilerView extends FrameLayout  implements ViewOptionsMenu, Select
 	TouchImageView spoilerImage;
 	ArrayList<Bitmap> lBitmaps;
 	loadProcessor processor;
-	
-	public SpoilerView(Context context, LayoutInflater inflater) {
+
+	public SpoilerView(Context context, LayoutInflater inflater)
+	{
 		super(context);
 		this.context = context;
 		lBitmaps = new ArrayList<Bitmap>();
-		
+
 		aktCache = null;
 
 		SelectedCacheEventList.Add(this);
-		
-		RelativeLayout spoilerLayout = (RelativeLayout)inflater.inflate(R.layout.spoilerview, null, false);
+
+		RelativeLayout spoilerLayout = (RelativeLayout) inflater.inflate(
+				R.layout.spoilerview, null, false);
 		this.addView(spoilerLayout);
-	
-        g = (Gallery) findViewById(R.id.spoilerGallery);
-        g.setSpacing(0);
-        spoilerFilename = (TextView) findViewById(R.id.spoilerFilename);
-        spoilerImage = new TouchImageView(context);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
-        params.addRule(RelativeLayout.BELOW, R.id.spoilerFilename);
-        params.addRule(RelativeLayout.ALIGN_BOTTOM);
-        spoilerImage.setLayoutParams(params);
-        spoilerLayout.addView(spoilerImage);
-		
-        g.setAdapter(new ImageAdapter(context));
-        g.setOnItemSelectedListener(this);
 
-        spoilerFilename.setTextColor(Color.BLACK);
-    }
+		g = (Gallery) findViewById(R.id.spoilerGallery);
+		g.setSpacing(0);
+		spoilerFilename = (TextView) findViewById(R.id.spoilerFilename);
+		spoilerImage = new TouchImageView(context);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.MATCH_PARENT,
+				RelativeLayout.LayoutParams.MATCH_PARENT);
+		params.addRule(RelativeLayout.BELOW, R.id.spoilerFilename);
+		params.addRule(RelativeLayout.ALIGN_BOTTOM);
+		spoilerImage.setLayoutParams(params);
+		spoilerLayout.addView(spoilerImage);
 
+		g.setAdapter(new ImageAdapter(context));
+		g.setOnItemSelectedListener(this);
+
+		spoilerFilename.setTextColor(Color.BLACK);
+	}
 
 	@Override
-	public boolean ItemSelected(MenuItem item) {
+	public boolean ItemSelected(MenuItem item)
+	{
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public void BeforeShowMenu(Menu menu) {
+	public void BeforeShowMenu(Menu menu)
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	Bitmap bmp;
 	String nextBitmap = "";
-	
+
 	@Override
-	public void onItemSelected(AdapterView parent, View v, int position, long id) {
-    //    mSwitcher.setImageResource(mImageIds[position]);
-		if(aktCache == null)
-			return;
-		String filename = GlobalCore.SelectedCache().SpoilerRessources().get(position);
+	public void onItemSelected(AdapterView parent, View v, int position, long id)
+	{
+		// mSwitcher.setImageResource(mImageIds[position]);
+		if (aktCache == null) return;
+		String filename = GlobalCore.SelectedCache().SpoilerRessources()
+				.get(position);
 		filename = filename.substring(0, filename.lastIndexOf("."));
 		filename = FileIO.GetFileNameWithoutExtension(filename);
-		if (filename.indexOf(aktCache.GcCode) == 0)
-			filename = filename.substring(aktCache.GcCode.length());
-		if (filename.indexOf(" - ") == 0)
-			filename = filename.substring(3);
+		if (filename.indexOf(aktCache.GcCode) == 0) filename = filename
+				.substring(aktCache.GcCode.length());
+		if (filename.indexOf(" - ") == 0) filename = filename.substring(3);
 		spoilerFilename.setText(filename);
-        String file = GlobalCore.SelectedCache().SpoilerRessources().get(position);
-		
+		String file = GlobalCore.SelectedCache().SpoilerRessources()
+				.get(position);
+
 		// das Laden sollte noch in einen Thread ausgelagert werden
-        nextBitmap = file;
-        if (processor == null)
-        {
-        	processor = new loadProcessor();
-        	processor.execute(file);
-        }
-//        bmp = BitmapFactory.decodeFile(file);
-//        spoilerImage.setImage(bmp, spoilerImage.getWidth(), spoilerImage.getHeight());
-    }
+		nextBitmap = file;
+		if (processor == null)
+		{
+			processor = new loadProcessor();
+			processor.execute(file);
+		}
+		// bmp = BitmapFactory.decodeFile(file);
+		// spoilerImage.setImage(bmp, spoilerImage.getWidth(),
+		// spoilerImage.getHeight());
+	}
 
-    public class ImageAdapter extends BaseAdapter {
-        public ImageAdapter(Context c) {
-            mContext = c;
-        }
+	public class ImageAdapter extends BaseAdapter
+	{
+		public ImageAdapter(Context c)
+		{
+			mContext = c;
+		}
 
-        public int getCount() {
-        	if (aktCache == null) return 0;
-        	if (aktCache.SpoilerExists())
-        		return aktCache.SpoilerRessources().size();
-        	else
-        		return 0;
-//            return mThumbIds.length;
-        }
+		public int getCount()
+		{
+			if (aktCache == null) return 0;
+			if (aktCache.SpoilerExists()) return aktCache.SpoilerRessources()
+					.size();
+			else
+				return 0;
+			// return mThumbIds.length;
+		}
 
-        public Object getItem(int position) {
-            return position;
-        }
+		public Object getItem(int position)
+		{
+			return position;
+		}
 
-        public long getItemId(int position) {
-            return position;
-        }
+		public long getItemId(int position)
+		{
+			return position;
+		}
 
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView i = new ImageView(mContext);
+		public View getView(int position, View convertView, ViewGroup parent)
+		{
+			ImageView i = new ImageView(mContext);
 
-            String file = GlobalCore.SelectedCache().SpoilerRessources().get(position);
-            Bitmap bit = null;
-       		bit = lBitmaps.get(position);
-            if (bit == null)
-            	return null;
-            i.setImageBitmap(bit);
-//            i.setImageResource(mThumbIds[position]);
-            i.setAdjustViewBounds(true);
-            i.setLayoutParams(new Gallery.LayoutParams(
-                    LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-//            i.setBackgroundResource(R.drawable.picture_frame);
-            return i;
-        }
+			String file = GlobalCore.SelectedCache().SpoilerRessources()
+					.get(position);
+			Bitmap bit = null;
+			bit = lBitmaps.get(position);
+			if (bit == null) return null;
+			i.setImageBitmap(bit);
+			// i.setImageResource(mThumbIds[position]);
+			i.setAdjustViewBounds(true);
+			i.setLayoutParams(new Gallery.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+			// i.setBackgroundResource(R.drawable.picture_frame);
+			return i;
+		}
 
-        private Context mContext;
+		private Context mContext;
 
-    }
-    
-    public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
-    
-    	int width = bm.getWidth();
-       	int height = bm.getHeight();
-    	float scaleWidth = ((float) newWidth) / width;
-    	float scaleHeight = ((float) newHeight) / height;
-    	if (scaleWidth > scaleHeight)
-    		scaleWidth = scaleHeight;
-    	else
-    		scaleHeight = scaleWidth;
-    	// create a matrix for the manipulation
-    	Matrix matrix = new Matrix();
-    	// resize the bit map
-    	matrix.postScale(scaleWidth, scaleHeight);
-    	// recreate the new Bitmap
-    	Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
-    	return resizedBitmap;
-    }
-    
+	}
+
+	public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth)
+	{
+
+		int width = bm.getWidth();
+		int height = bm.getHeight();
+		float scaleWidth = ((float) newWidth) / width;
+		float scaleHeight = ((float) newHeight) / height;
+		if (scaleWidth > scaleHeight) scaleWidth = scaleHeight;
+		else
+			scaleHeight = scaleWidth;
+		// create a matrix for the manipulation
+		Matrix matrix = new Matrix();
+		// resize the bit map
+		matrix.postScale(scaleWidth, scaleHeight);
+		// recreate the new Bitmap
+		Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height,
+				matrix, false);
+		return resizedBitmap;
+	}
+
 	@Override
-	public void SelectedCacheChanged(Cache cache, Waypoint waypoint) {
+	public void SelectedCacheChanged(Cache cache, Waypoint waypoint)
+	{
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void OnShow() {
+	public void OnShow()
+	{
 		// TODO Auto-generated method stub
 		Logger.DEBUG("sv1");
 		aktCache = GlobalCore.SelectedCache();
@@ -203,91 +224,106 @@ public class SpoilerView extends FrameLayout  implements ViewOptionsMenu, Select
 				Logger.DEBUG("sv5");
 				bmp.recycle();
 				Logger.DEBUG("sv6");
-			} catch (Exception exc)
+			}
+			catch (Exception exc)
 			{
 				Logger.Error("SpoilerView.onShow()", "AddBitmap", exc);
 			}
 		}
 		Logger.DEBUG("sv7");
-        g.setAdapter(new ImageAdapter(context));		
+		g.setAdapter(new ImageAdapter(context));
 		Logger.DEBUG("sv8");
 	}
 
-
 	@Override
-	public void OnHide() {
+	public void OnHide()
+	{
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public void OnFree() {
-		
+	public void OnFree()
+	{
+
 	}
 
-
 	@Override
-	public int GetMenuId() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-
-	@Override
-	public void onNothingSelected(AdapterView<?> arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public void ActivityResult(int requestCode, int resultCode, Intent data) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public int GetContextMenuId() {
+	public int GetMenuId()
+	{
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-
 	@Override
-	public void BeforeShowContextMenu(Menu menu) {
+	public void onNothingSelected(AdapterView<?> arg0)
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
+	public void ActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		// TODO Auto-generated method stub
+
+	}
 
 	@Override
-	public boolean ContextMenuItemSelected(MenuItem item) {
+	public int GetContextMenuId()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void BeforeShowContextMenu(Menu menu)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean ContextMenuItemSelected(MenuItem item)
+	{
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	
-    private class loadProcessor extends AsyncTask<String, Integer, Integer> {
-    	Bitmap bmp = null;
+	private class loadProcessor extends AsyncTask<String, Integer, Integer>
+	{
+		Bitmap bmp = null;
+
 		@Override
-		protected Integer doInBackground(String... params) {
-	        bmp = BitmapFactory.decodeFile(params[0]);
-	        if (params[0].equalsIgnoreCase(nextBitmap))
-	        	nextBitmap = "";
+		protected Integer doInBackground(String... params)
+		{
+			if (!(params == null || params[0].equals("")))
+			{
+				File testingFile = new File(params[0]);
+				if (testingFile.exists())
+				{
+					System.gc();
+					bmp = BitmapFactory.decodeFile(params[0]);
+					if (params[0].equalsIgnoreCase(nextBitmap)) nextBitmap = "";
+				}
+			}
+
 			return null;
 		}
-		
-		protected void onPostExecute(Integer result) {
-			if (bmp != null)				
-				spoilerImage.setImage(bmp, spoilerImage.getWidth(), spoilerImage.getHeight());			
+
+		protected void onPostExecute(Integer result)
+		{
+			if (bmp != null) spoilerImage.setImage(bmp,
+					spoilerImage.getWidth(), spoilerImage.getHeight());
 			processor = null;
+			System.gc();
+
 			if (!(nextBitmap.equals("")))
 			{
-	        	processor = new loadProcessor();
-	        	processor.execute(nextBitmap);				
+				processor = new loadProcessor();
+				processor.execute(nextBitmap);
 			}
-		}    	
+		}
 
-    }
+	}
 
 }
