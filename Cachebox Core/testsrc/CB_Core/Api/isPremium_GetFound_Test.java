195ea61a2f1fb487659827d5b9fdece59c7d867d
longri <www.longri.de@googlemail.com>
2011-09-24 19:24:48 +0000

    + add JUnit API Test 	Your personal Config file there must lie in the folder "trunk\Cachebox Core\testdata".         This file is on the ignore list of SVN, so that she cannot be published personal config.         (for the protection of the personal API Keys)
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@439 5890f383-b09f-485f-8f59-22bc967d67fd

---
+++ Cachebox Core/testsrc/CB_Core/Api/isPremium_GetFound_Test.java
@@ -0,0 +1,64 @@
+package CB_Core.Api;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import CB_Core.Config;
+import CB_Core.Map.Descriptor;
+import CB_Core.Types.Cache;
+import CB_Core.Types.LogEntry;
+import CB_Core.Types.MeasuredCoordList;
+
+import junit.framework.TestCase;
+
+/**
+ * Enthält die Tests um die Caches zu einer bestimmten Position über die API
+ * abzufragen
+ * 
+ * @author Longri
+ */
+public class isPremium_GetFound_Test extends TestCase
+{
+
+	
+	@Override
+	public void setUp() throws Exception
+	{
+		super.setUp();
+		LoadConfig();
+	}
+
+	@Override
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+
+	}
+
+	/**
+	 * lädt die Config Datei aus dem Ordner "trunk\Cachebox Core\testdata" Hie
+	 * muss eine gültige cachebox.config Datei liegen. Diese Datei ist auf der
+	 * Ignore list von SVN, so das diese Persönliche config nicht veröffentlicht
+	 * werden kann. (zum Schutz des Persönlichen API Keys)
+	 */
+	private void LoadConfig()
+	{
+		Config.Initialize("./testdata/", "./testdata/cachebox.config");
+	}
+
+	public void testIsPremiumMember()
+	{
+		assertTrue("Muss Premium Member sein", GroundspeakAPI.IsPremiumMember(Config.GetString("GcAPI")));
+	}
+	
+	public void testGetCachesFound()
+	{
+		
+		int Anzahl = GroundspeakAPI.GetCachesFound(Config.GetString("GcAPI"));
+		
+		//Bei mir
+		assertTrue("Muss Anzahl meiner Funde sein (" + Anzahl + ")", Anzahl == 145);
+	}
+
+}
