1c4ba59cf276bec6f879d4d07f5f718cbe532e3a
longri <www.longri.de@googlemail.com>
2012-08-02 08:39:43 +0000

    + add OpenGL-TrackListView
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@983 5890f383-b09f-485f-8f59-22bc967d67fd

--- Cachebox Core/src/CB_Core/GL_UI/Views/TrackListView.java
+++ Cachebox Core/src/CB_Core/GL_UI/Views/TrackListView.java
@@ -1,28 +1,42 @@
 package CB_Core.GL_UI.Views;
 
+import java.util.Iterator;
+
 import CB_Core.GlobalCore;
-import CB_Core.Plattform;
 import CB_Core.Events.platformConector;
-import CB_Core.GL_UI.CB_View_Base;
 import CB_Core.GL_UI.Fonts;
+import CB_Core.GL_UI.GL_View_Base;
 import CB_Core.GL_UI.SpriteCache;
 import CB_Core.GL_UI.ViewConst;
-import CB_Core.GL_UI.Controls.Label;
+import CB_Core.GL_UI.Controls.List.Adapter;
+import CB_Core.GL_UI.Controls.List.ListViewItemBase;
+import CB_Core.GL_UI.Controls.List.V_ListView;
+import CB_Core.GL_UI.Views.TrackListViewItem.RouteChangedListner;
+import CB_Core.Map.RouteOverlay;
+import CB_Core.Map.RouteOverlay.Track;
 import CB_Core.Math.CB_RectF;
+import CB_Core.Math.UiSizes;
 
-public class TrackListView extends CB_View_Base
+import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
+import com.badlogic.gdx.graphics.g2d.BitmapFontCache;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class TrackListView extends V_ListView
 {
+	public static CB_RectF ItemRec;
+	BitmapFontCache emptyMsg;
 
 	public TrackListView(CB_RectF rec, String Name)
 	{
 		super(rec, Name);
 
-		Label lblDummy = new Label(CB_RectF.ScaleCenter(rec, 0.8f), "DummyLabel");
-		lblDummy.setFont(Fonts.getNormal());
-		lblDummy.setText("Dummy TrackListView");
+		ItemRec = new CB_RectF(0, 0, this.width, UiSizes.getButtonHeight() * 1.1f);
+
 		setBackground(SpriteCache.ListBack);
 
-		if (GlobalCore.platform == Plattform.Desktop) this.addChild(lblDummy);
+		this.setBaseAdapter(null);
+		this.setBaseAdapter(new CustomAdapter());
 
 	}
 
@@ -46,10 +60,89 @@ public class TrackListView extends CB_View_Base
 	}
 
 	@Override
+	public void render(SpriteBatch batch)
+	{
+		// if Track List empty, draw empty Msg
+		if (RouteOverlay.Routes == null || RouteOverlay.Routes.size() == 0)
+		{
+			if (emptyMsg == null)
+			{
+				emptyMsg = new BitmapFontCache(Fonts.getBig());
+				TextBounds bounds = emptyMsg.setText(GlobalCore.Translations.Get("EmptyTrackList"), 0, 0);
+				emptyMsg.setPosition(this.halfWidth - (bounds.width / 2), this.halfHeight - (bounds.height / 2));
+			}
+			if (emptyMsg != null) emptyMsg.draw(batch, 0.5f);
+		}
+		else
+		{
+			super.render(batch);
+		}
+	}
+
+	@Override
 	protected void SkinIsChanged()
 	{
 		// TODO Auto-generated method stub
 
+	}
+
+	public class CustomAdapter implements Adapter
+	{
+
+		public CustomAdapter()
+		{
+		}
+
+		@Override
+		public int getCount()
+		{
+			return RouteOverlay.Routes.size();
+		}
+
+		@Override
+		public ListViewItemBase getView(int position)
+		{
+			TrackListViewItem v = new TrackListViewItem(ItemRec, position, RouteOverlay.Routes.get(position), new RouteChangedListner()
+			{
+
+				@Override
+				public void RouteChanged(Track route)
+				{
+					// Notify Map to Reload RouteOverlay
+					RouteOverlay.RoutesChanged();
+				}
+			});
+			return v;
+		}
+
+		@Override
+		public float getItemSize(int position)
+		{
+			return ItemRec.getHeight();
+		}
+
+	}
+
+	@Override
+	public boolean onTouchDown(int x, int y, int pointer, int button)
+	{
+		super.onTouchDown(x, y, pointer, button);
+
+		// for (Iterator<GL_View_Base> iterator = childs.iterator(); iterator.hasNext();)
+		for (Iterator<GL_View_Base> iterator = childs.reverseIterator(); iterator.hasNext();)
+		{
+			// Child View suchen, innerhalb derer Bereich der touchDown statt gefunden hat.
+			GL_View_Base view = iterator.next();
+
+			if (view instanceof TrackListViewItem)
+			{
+				if (view.contains(x, y))
+				{
+					((TrackListViewItem) view).lastItemTouchPos = new Vector2(x - view.getPos().x, y - view.getPos().y);
+				}
+			}
+		}
+		return true;
 	}
 
 }
---
+++ Cachebox Core/src/CB_Core/GL_UI/Views/TrackListViewItem.java
@@ -0,0 +1,169 @@
+package CB_Core.GL_UI.Views;
+
+import CB_Core.GL_UI.SpriteCache;
+import CB_Core.GL_UI.Controls.Dialog;
+import CB_Core.GL_UI.Controls.Label;
+import CB_Core.GL_UI.Controls.List.ListViewItemBackground;
+import CB_Core.GL_UI.GL_Listener.GL_Listener;
+import CB_Core.Map.RouteOverlay.Track;
+import CB_Core.Math.CB_RectF;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class TrackListViewItem extends ListViewItemBackground
+{
+	private static Sprite chkOff;
+	private static Sprite chkOn;
+
+	private static CB_RectF lBounds;
+	private static CB_RectF rBounds;
+	private static CB_RectF rChkBounds;
+
+	// private Member
+	private Track mRoute;
+	private float left;
+	private float top;
+
+	private Label EntryName;
+	private Sprite colorReck;
+	private boolean Clicked = false;
+	public Vector2 lastItemTouchPos;
+
+	private RouteChangedListner mRouteChangedListner;
+
+	public interface RouteChangedListner
+	{
+		public void RouteChanged(Track route);
+	}
+
+	public TrackListViewItem(CB_RectF rec, int Index, Track route, RouteChangedListner listner)
+	{
+		super(rec, Index, route.Name);
+		mRoute = route;
+		mRouteChangedListner = listner;
+	}
+
+	@Override
+	protected void SkinIsChanged()
+	{
+
+	}
+
+	@Override
+	protected void render(SpriteBatch batch)
+	{
+
+		super.render(batch);
+
+		boolean rClick = false;
+		boolean lClick = false;
+		if (this.isPressed)
+		{
+			lClick = lBounds.contains(this.lastItemTouchPos);
+			rClick = rBounds.contains(this.lastItemTouchPos);
+
+			if (lClick || rClick) Clicked = true;
+			isPressed = GL_Listener.isTouchDown();
+		}
+		else
+		{
+			if (Clicked)
+			{
+				Clicked = false;
+				lClick = lBounds.contains(this.lastItemTouchPos);
+				rClick = rBounds.contains(this.lastItemTouchPos);
+				if (lClick) colorClick();
+				if (rClick) chkClick();
+			}
+		}
+
+		// initial
+		left = LeftWidth;
+		top = this.height - TopHight;
+
+		drawColorRec(batch);
+
+		// draw Name
+		if (EntryName == null)
+		{
+
+			CB_RectF rec = new CB_RectF(left, 0, this.width - left - height - 10, this.height);
+			EntryName = new Label(rec, "");
+
+			EntryName.setText(mRoute.Name);
+
+			this.addChild(EntryName);
+		}
+
+		drawRightChkBox(batch);
+
+	}
+
+	private void drawColorRec(SpriteBatch batch)
+	{
+		if (lBounds == null)
+		{
+			lBounds = new CB_RectF(0, 0, height, height);
+			lBounds = lBounds.ScaleCenter(0.95f);
+		}
+
+		if (colorReck == null)
+		{
+			colorReck = SpriteCache.getThemedSprite("text-field-back");
+			colorReck.setBounds(lBounds.getX(), lBounds.getY(), lBounds.getWidth(), lBounds.getHeight());
+			colorReck.setColor(mRoute.getColor());
+		}
+
+		colorReck.draw(batch);
+
+		left += lBounds.getWidth() + Dialog.margin;
+
+	}
+
+	private void drawRightChkBox(SpriteBatch batch)
+	{
+		if (rBounds == null || rChkBounds == null)
+		{
+			rBounds = new CB_RectF(width - height - 10, 5, height - 10, height - 10);// = right Button bounds
+
+			rChkBounds = rBounds.ScaleCenter(0.8f);
+		}
+
+		if (chkOff == null)
+		{
+			chkOff = SpriteCache.getThemedSprite("check-off");
+			chkOff.setBounds(rChkBounds.getX(), rChkBounds.getY(), rChkBounds.getWidth(), rChkBounds.getHeight());
+		}
+
+		if (chkOn == null)
+		{
+			chkOn = SpriteCache.getThemedSprite("check-on");
+			chkOn.setBounds(rChkBounds.getX(), rChkBounds.getY(), rChkBounds.getWidth(), rChkBounds.getHeight());
+		}
+
+		if (mRoute.ShowRoute)
+		{
+			chkOn.draw(batch);
+		}
+		else
+		{
+			chkOff.draw(batch);
+		}
+
+	}
+
+	private void chkClick()
+	{
+		mRoute.ShowRoute = !mRoute.ShowRoute;
+		if (mRouteChangedListner != null) mRouteChangedListner.RouteChanged(mRoute);
+	}
+
+	private void colorClick()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
