52260a6d4f3cee8c8cf83bffa78b12b39235b410
longri <www.longri.de@googlemail.com>
2012-11-17 08:17:13 +0000

    + changelog + Increase Version Code at manifest
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@1214 5890f383-b09f-485f-8f59-22bc967d67fd

--- Cachebox Core/src/CB_Core/GlobalCore.java
+++ Cachebox Core/src/CB_Core/GlobalCore.java
@@ -14,7 +14,7 @@ import com.badlogic.gdx.utils.Clipboard;
 public class GlobalCore
 {
 
-	public static final int CurrentRevision = 1211;
+	public static final int CurrentRevision = 1214;
 	public static final String CurrentVersion = "0.5.";
 	public static final String VersionPrefix = "BETA";
 
