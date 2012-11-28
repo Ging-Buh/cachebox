a0172e361e0a5055db0f39711d29e31714848df4
arbor95 <arbor95>
2012-11-28 20:11:35 +0000

    Version to 1248
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@1248 5890f383-b09f-485f-8f59-22bc967d67fd

--- Cachebox Core/src/CB_Core/GlobalCore.java
+++ Cachebox Core/src/CB_Core/GlobalCore.java
@@ -14,7 +14,7 @@ import com.badlogic.gdx.utils.Clipboard;
 public class GlobalCore
 {
 
-	public static final int CurrentRevision = 1245;
+	public static final int CurrentRevision = 1248;
 	public static final String CurrentVersion = "0.6.";
 	public static final String VersionPrefix = "Test";
 
