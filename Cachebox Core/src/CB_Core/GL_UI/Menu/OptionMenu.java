cd59f59e6a9b03b01ee39a546051427de48336a7
longri <www.longri.de@googlemail.com>
2013-01-10 07:41:23 +0000

    + forgotten file
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@1332 5890f383-b09f-485f-8f59-22bc967d67fd

---
+++ Cachebox Core/src/CB_Core/GL_UI/Menu/OptionMenu.java
@@ -0,0 +1,55 @@
+package CB_Core.GL_UI.Menu;
+
+import CB_Core.GL_UI.GL_View_Base;
+import CB_Core.GL_UI.Controls.MessageBox.GL_MsgBox;
+import CB_Core.GL_UI.Controls.MessageBox.MessageBoxButtons;
+import CB_Core.GL_UI.GL_Listener.GL;
+
+public class OptionMenu extends Menu
+{
+
+	public OptionMenu(String Name)
+	{
+		super(Name);
+		this.setButtonCaptions(MessageBoxButtons.OK);
+		this.mMsgBoxClickListner = new GL_MsgBox.OnMsgBoxClickListener()
+		{
+
+			@Override
+			public boolean onClick(int which)
+			{
+				GL.that.closeDialog(that);
+				return true;
+			}
+		};
+
+		MenuItemClickListner = new OnClickListener()
+		{
+
+			@Override
+			public boolean onClick(GL_View_Base v, int x, int y, int pointer, int button)
+			{
+
+				if (v instanceof MenuItem)
+				{
+					MenuItem tmp = (MenuItem) v;
+					if (tmp.isCheckable())
+					{
+						tmp.toggleCheck();
+					}
+				}
+
+				if (mOnItemClickListner != null)
+				{
+					for (OnClickListener tmp : mOnItemClickListner)
+					{
+						if (tmp.onClick(v, x, y, pointer, button)) break;
+					}
+				}
+
+				return true;
+			}
+		};
+	}
+
+}
