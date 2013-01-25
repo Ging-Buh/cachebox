f60dd8ce8a77ef59900fc9e2d6e4d798d21f6c56
Hubert Merkle <hubert@fmby.de>
2013-01-25 22:27:16 +0000

    Increased Revision Number
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@1385 5890f383-b09f-485f-8f59-22bc967d67fd

--- Cachebox Core/src/CB_Core/GlobalCore.java
+++ Cachebox Core/src/CB_Core/GlobalCore.java
@@ -15,7 +15,7 @@ import com.badlogic.gdx.utils.Clipboard;
 public class GlobalCore
 {
 
-	public static final int CurrentRevision = 1383;
+	public static final int CurrentRevision = 1385;
 	public static final String CurrentVersion = "0.5.";
 	public static final String VersionPrefix = "Test";
 
