8531657d9d2e6c9fde519b095649c8895b9ac611
Hubert Merkle <hubert@fmby.de>
2013-02-17 21:14:05 +0000

    Copied Database Table for Trackables and TBLogs into FieldNotes Database
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@1456 5890f383-b09f-485f-8f59-22bc967d67fd

--- Cachebox Core/src/CB_Core/DB/Database.java
+++ Cachebox Core/src/CB_Core/DB/Database.java
@@ -335,6 +335,14 @@ public abstract class Database
 				{
 					execSQL("ALTER TABLE [FieldNotes] ADD COLUMN [GC_Vote] integer default 0");
 				}
+				if (lastDatabaseSchemeVersion < 1004)
+				{
+					execSQL("CREATE TABLE [Trackable] ([Id] integer not null primary key autoincrement, [Archived] bit NULL, [GcCode] nvarchar (15) NULL, [CacheId] bigint NULL, [CurrentGoal] ntext, [CurrentOwnerName] nvarchar (255) NULL, [DateCreated] datetime NULL, [Description] ntext, [IconUrl] nvarchar (255) NULL, [ImageUrl] nvarchar (255) NULL, [name] nvarchar (255) NULL, [OwnerName] nvarchar (255), [Url] nvarchar (255) NULL);");
+					execSQL("CREATE INDEX [cacheid_idx] ON [Trackable] ([CacheId] ASC);");
+					execSQL("CREATE TABLE [TbLogs] ([Id] integer not null primary key autoincrement, [TrackableId] integer not NULL, [CacheID] bigint NULL, [GcCode] nvarchar (15) NULL, [LogIsEncoded] bit NULL DEFAULT 0, [LogText] ntext, [LogTypeId] bigint NULL, [LoggedByName] nvarchar (255) NULL, [Visited] datetime NULL);");
+					execSQL("CREATE INDEX [trackableid_idx] ON [TbLogs] ([TrackableId] ASC);");
+					execSQL("CREATE INDEX [trackablecacheid_idx] ON [TBLOGS] ([CacheId] ASC);");
+				}
 				setTransactionSuccessful();
 			}
 			catch (Exception exc)
