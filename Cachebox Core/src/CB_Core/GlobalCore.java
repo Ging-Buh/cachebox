8d9923662f14c7df1f25fe9ca0e84969e95b1e7e
Hubert Merkle <hubert@fmby.de>
2013-03-29 21:51:34 +0000

    Fixed Problem with BSH-Maps in ACB
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@1532 5890f383-b09f-485f-8f59-22bc967d67fd

--- Cachebox Core/src/CB_Core/GlobalCore.java
+++ Cachebox Core/src/CB_Core/GlobalCore.java
@@ -14,7 +14,7 @@ import com.badlogic.gdx.utils.Clipboard;
 public class GlobalCore
 {
 
-	public static final int CurrentRevision = 1531;
+	public static final int CurrentRevision = 1532;
 	public static final String CurrentVersion = "0.5.";
 	public static final String VersionPrefix = "Test";
 
