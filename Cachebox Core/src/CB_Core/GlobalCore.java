7b258db3698b21759c1fad5abf509895845dcb8f
longri <www.longri.de@googlemail.com>
2013-04-26 08:05:34 +0000

    + merge Trunk with Bransh and create new APK’s
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@1601 5890f383-b09f-485f-8f59-22bc967d67fd

--- Cachebox Core/src/CB_Core/GlobalCore.java
+++ Cachebox Core/src/CB_Core/GlobalCore.java
@@ -14,7 +14,7 @@ import com.badlogic.gdx.utils.Clipboard;
 public class GlobalCore
 {
 
-	public static final int CurrentRevision = 1599;
+	public static final int CurrentRevision = 1601;
 	public static final String CurrentVersion = "0.6.";
 	public static final String VersionPrefix = "Test";
 
