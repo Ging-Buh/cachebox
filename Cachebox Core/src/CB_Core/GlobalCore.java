4a8291309eba570f91e88a83e760495bc4a69915
arbor95 <arbor95>
2013-06-04 17:06:46 +0000

    setting Version to 1706
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@1706 5890f383-b09f-485f-8f59-22bc967d67fd

--- Cachebox Core/src/CB_Core/GlobalCore.java
+++ Cachebox Core/src/CB_Core/GlobalCore.java
@@ -22,7 +22,7 @@ import com.badlogic.gdx.utils.Clipboard;
 public class GlobalCore
 {
 
-	public static final int CurrentRevision = 1700;
+	public static final int CurrentRevision = 1706;
 	public static final String CurrentVersion = "0.6.";
 	public static final String VersionPrefix = "Test";
 
