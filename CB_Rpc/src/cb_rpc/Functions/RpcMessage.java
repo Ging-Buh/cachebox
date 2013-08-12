d74bcde7c842994b5dd1edda002e0ffadf1b811d
Hubert Merkle <hubert@fmby.de>
2013-08-12 20:10:12 +0000

    + WCB adjustments
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@1782 5890f383-b09f-485f-8f59-22bc967d67fd

---
+++ CB_Rpc/src/cb_rpc/Functions/RpcMessage.java
@@ -0,0 +1,9 @@
+package cb_rpc.Functions;
+
+import java.io.Serializable;
+
+public class RpcMessage implements Serializable {
+
+	private static final long serialVersionUID = 4037342703159211880L;
+
+}
