e29b24426eab090282fdb67990efc411ee5337ae
longri <www.longri.de@googlemail.com>
2014-03-28 14:30:44 +0000

    + fix NPE with Mapdownload + add TimeOut for MapDownload + refresh translations and fix typo
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@2039 5890f383-b09f-485f-8f59-22bc967d67fd

--- CB_UI/src/CB_UI/GL_UI/Activitys/MapDownload.java
+++ CB_UI/src/CB_UI/GL_UI/Activitys/MapDownload.java
@@ -1,23 +1,19 @@
 package CB_UI.GL_UI.Activitys;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.conn.ConnectTimeoutException;
 
+import CB_Core.Api.GroundspeakAPI;
 import CB_Core.Import.BreakawayImportThread;
 import CB_Locator.Map.ManagerBase;
 import CB_Translation_Base.TranslationEngine.Translation;
@@ -35,6 +31,7 @@ import CB_UI_Base.GL_UI.Controls.MessageBox.GL_MsgBox;
 import CB_UI_Base.GL_UI.Controls.MessageBox.GL_MsgBox.OnMsgBoxClickListener;
 import CB_UI_Base.GL_UI.Controls.MessageBox.MessageBoxButtons;
 import CB_UI_Base.GL_UI.Controls.MessageBox.MessageBoxIcon;
+import CB_UI_Base.GL_UI.Controls.PopUps.ConnectionError;
 import CB_UI_Base.GL_UI.GL_Listener.GL;
 import CB_UI_Base.Math.CB_RectF;
 import CB_UI_Base.Math.UI_Size_Base;
@@ -330,7 +327,7 @@ public class MapDownload extends ActivityBase implements ProgressChangedEvent
 			dis = new ImportAnimation(scrollBox);
 			dis.setBackground(getBackground());
 			dis.setAnimationType(AnimationType.Download);
-			lblProgressMsg.setText(Translation.Get("ChkAvilebleMaps"));
+			lblProgressMsg.setText(Translation.Get("ChkAvailableMaps"));
 			this.addChild(dis, false);
 			bOK.disable();
 
@@ -351,38 +348,27 @@ public class MapDownload extends ActivityBase implements ProgressChangedEvent
 			public void run()
 			{
 				// Read XML
-				HttpClient httpclient = new DefaultHttpClient();
+
 				HttpGet httpget = new HttpGet(URL_FREIZEITKARTE);
 
 				httpget.setHeader("Accept", "application/json");
 				httpget.setHeader("Content-type", "application/json");
 
-				HttpResponse response;
 				try
 				{
-					response = httpclient.execute(httpget);
-
-					BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-					String line = "";
-					while ((line = rd.readLine()) != null)
-					{
-						repository_freizeitkarte_android += line + "\n";
-					}
-
+					repository_freizeitkarte_android = GroundspeakAPI.Execute(httpget);
 				}
-				catch (UnknownHostException e)
+				catch (ConnectTimeoutException e)
 				{
-					e.printStackTrace();
+					GL.that.Toast(ConnectionError.INSTANCE);
 				}
 				catch (ClientProtocolException e)
 				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					GL.that.Toast(ConnectionError.INSTANCE);
 				}
 				catch (IOException e)
 				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					GL.that.Toast(ConnectionError.INSTANCE);
 				}
 
 				fillDownloadList();
@@ -431,7 +417,7 @@ public class MapDownload extends ActivityBase implements ProgressChangedEvent
 
 		// Create possible download List
 
-		for (int i = mapInfoList.size(); i > 0; i--)
+		for (int i = 0, n = mapInfoList.size(); i < n; i++)
 		{
 			MapRepositoryInfo map = mapInfoList.get(i);
 
