c2a42c242d2163eb402c6f26f36bd53bacdf61fe
Hubert Merkle <hubert@fmby.de>
2014-05-29 05:55:49 +0000

    Improved Server Import Speed with Database Transaction
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@2144 5890f383-b09f-485f-8f59-22bc967d67fd

--- CB_Core/src/CB_Core/Import/ImportCBServer.java
+++ CB_Core/src/CB_Core/Import/ImportCBServer.java
@@ -30,6 +30,7 @@ public class ImportCBServer
 	public void importCBServer(ArrayList<ListItem> cbServerExportList, ImporterProgress ip, boolean importImages)
 	{
 		long startTS = System.currentTimeMillis();
+		long tmpTS = System.currentTimeMillis();
 		int count = 0;
 		int anzToLoad = 100; // Anzahl der Caches, die auf einmal geladen werden sollen
 		int anzDownloadsTotal = 0;
@@ -46,148 +47,163 @@ public class ImportCBServer
 		ip.setJobMax("importCBServer", anzDownloadsTotal);
 		int actDownload = 0;
 
-		for (ListItem item : cbServerExportList)
+		Database.Data.beginTransaction();
+		try
 		{
-			if (!item.getDownload()) continue;
-			// Import
-			RpcClientCB rpc = new RpcClientCB();
-			boolean dataAvailable = true;
-			int startIndex = 0;
-
-			while (dataAvailable)
+			for (ListItem item : cbServerExportList)
 			{
-				actDownload++;
-				ip.ProgressInkrement("importCBServer", actDownload + "-" + anzDownloadsTotal + " | " + item.getDescription(), false);
-				RpcAnswer answer = rpc.sendRpcToServer(new RpcMessage_GetCacheList(item.getId(), startIndex, anzToLoad));
-				if (answer instanceof RpcAnswer_GetCacheList)
+				if (!item.getDownload()) continue;
+				// Import
+				RpcClientCB rpc = new RpcClientCB();
+				boolean dataAvailable = true;
+				int startIndex = 0;
+
+				while (dataAvailable)
 				{
-					RpcAnswer_GetCacheList gclAnswer = (RpcAnswer_GetCacheList) answer;
-					System.out.println("************* CacheList ***************");
-					// GPX-Filename und Category Eintrag prüfen
-					Category cat = null;
-					CategoryDAO catDao = new CategoryDAO();
-					// Suchen, ob diese Category schon vorhanden ist
-					for (int i = 0, n = CoreSettingsForward.Categories.size(); i < n; i++)
+					actDownload++;
+					ip.ProgressInkrement("importCBServer", actDownload + "-" + anzDownloadsTotal + " | " + item.getDescription(), false);
+					System.out.println("Load from Server (" + (System.currentTimeMillis() - tmpTS) + "ms)");
+					tmpTS = System.currentTimeMillis();
+
+					RpcAnswer answer = rpc.sendRpcToServer(new RpcMessage_GetCacheList(item.getId(), startIndex, anzToLoad));
+					System.out.println("Load from Server finished (" + (System.currentTimeMillis() - tmpTS) + "ms)");
+					tmpTS = System.currentTimeMillis();
+					if (answer instanceof RpcAnswer_GetCacheList)
 					{
-						Category tcat = CoreSettingsForward.Categories.get(i);
-						if (tcat.GpxFilename.equals(item.getDescription()))
+						RpcAnswer_GetCacheList gclAnswer = (RpcAnswer_GetCacheList) answer;
+						System.out.println("************* CacheList ***************");
+						// GPX-Filename und Category Eintrag prüfen
+						Category cat = null;
+						CategoryDAO catDao = new CategoryDAO();
+						// Suchen, ob diese Category schon vorhanden ist
+						for (int i = 0, n = CoreSettingsForward.Categories.size(); i < n; i++)
 						{
-							cat = tcat;
-							break;
-						}
-					}
-					// Wenn die Category noch nicht vorhanden ist -> erzeugen
-					if (cat == null)
-					{
-						cat = catDao.CreateNewCategory(item.getDescription());
-						CoreSettingsForward.Categories.add(cat); // Category hinzufügen
-					}
-					// GpxFilenames Eintrag erzeugen
-					// Alle importierten Caches werdem diesem neuen GpxFilename zugeordnet
-					GpxFilename gpxFilename = catDao.CreateNewGpxFilename(cat, item.getDescription());
-
-					CacheDAO dao = new CacheDAO();
-					WaypointDAO wayDao = new WaypointDAO();
-					LogDAO logDAO = new LogDAO();
-
-					for (int i = 0, n = gclAnswer.getCacheList().size(); i < n; i++)
-					{
-						Cache cache = gclAnswer.getCacheList().get(i);
-						System.out.println(cache.getName());
-						cache.GPXFilename_ID = gpxFilename.Id;
-
-						dao.WriteToDatabase(cache);
-						if ((cache.getTmpNote() != null) && (cache.getTmpNote().length() > 0))
-						{
-							cache.setNoteChecksum((int) SDBM_Hash.sdbm(cache.getTmpNote()));
-							Parameters args = new Parameters();
-							// orginal NoteChecksum in DB speichern
-							args.put("Notes", cache.getTmpNote());
-							Database.Data.update("Caches", args, "id=" + cache.Id, null);
-
-							cache.setTmpNote(null);
-						}
-						if (cache.getTmpSolver() != null)
-						{
-							cache.setSolverChecksum((int) SDBM_Hash.sdbm(cache.getTmpSolver()));
-							Parameters args = new Parameters();
-							args.put("Solver", cache.getTmpSolver());
-							Database.Data.update("Caches", args, "id=" + cache.Id, null);
-
-							cache.setTmpSolver(null);
-						}
-
-						for (int j = 0, m = cache.waypoints.size(); j < m; j++)
-						{
-							Waypoint waypoint = (Waypoint) cache.waypoints.get(j);
-							wayDao.WriteToDatabase(waypoint);
-						}
-						if (importImages)
-						{
-							for (int j = 0, m = cache.getSpoilerRessources().size(); j < m; j++)
+							Category tcat = CoreSettingsForward.Categories.get(i);
+							if (tcat.GpxFilename.equals(item.getDescription()))
 							{
-								ImageEntry image = cache.getSpoilerRessources().get(j);
-								String url = CB_Rpc_Settings.CBS_IP.getValue();
-								int pos = url.indexOf(":");
-								if (pos >= 0)
-								{
-									url = url.substring(0, pos); // Port abschneiden, da dieser bereits in der imageURL der Images steht
-								}
-								url = "http://" + url + image.ImageUrl;
-								File file = new File(image.LocalPath);
-								url += cache.getGcCode().substring(0, 4) + "/";
+								cat = tcat;
+								break;
+							}
+						}
+						// Wenn die Category noch nicht vorhanden ist -> erzeugen
+						if (cat == null)
+						{
+							cat = catDao.CreateNewCategory(item.getDescription());
+							CoreSettingsForward.Categories.add(cat); // Category hinzufügen
+						}
+						// GpxFilenames Eintrag erzeugen
+						// Alle importierten Caches werdem diesem neuen GpxFilename zugeordnet
+						GpxFilename gpxFilename = catDao.CreateNewGpxFilename(cat, item.getDescription());
 
-								url += file.getName().replace(" ", "%20");
+						CacheDAO dao = new CacheDAO();
+						WaypointDAO wayDao = new WaypointDAO();
+						LogDAO logDAO = new LogDAO();
 
-								String imagePath;
-								if (image.ImageUrl.indexOf("/spoilers/") >= 0)
+						for (int i = 0, n = gclAnswer.getCacheList().size(); i < n; i++)
+						{
+							Cache cache = gclAnswer.getCacheList().get(i);
+							// System.out.println(cache.getName());
+							cache.GPXFilename_ID = gpxFilename.Id;
+
+							dao.WriteToDatabase(cache);
+							if ((cache.getTmpNote() != null) && (cache.getTmpNote().length() > 0))
+							{
+								cache.setNoteChecksum((int) SDBM_Hash.sdbm(cache.getTmpNote()));
+								Parameters args = new Parameters();
+								// orginal NoteChecksum in DB speichern
+								args.put("Notes", cache.getTmpNote());
+								Database.Data.update("Caches", args, "id=" + cache.Id, null);
+
+								cache.setTmpNote(null);
+							}
+							if (cache.getTmpSolver() != null)
+							{
+								cache.setSolverChecksum((int) SDBM_Hash.sdbm(cache.getTmpSolver()));
+								Parameters args = new Parameters();
+								args.put("Solver", cache.getTmpSolver());
+								Database.Data.update("Caches", args, "id=" + cache.Id, null);
+
+								cache.setTmpSolver(null);
+							}
+
+							for (int j = 0, m = cache.waypoints.size(); j < m; j++)
+							{
+								Waypoint waypoint = (Waypoint) cache.waypoints.get(j);
+								wayDao.WriteToDatabase(waypoint);
+							}
+							if (importImages)
+							{
+								for (int j = 0, m = cache.getSpoilerRessources().size(); j < m; j++)
 								{
-									imagePath = CB_Core_Settings.SpoilerFolder.getValue() + "/" + cache.getGcCode().substring(0, 4) + "/"
-											+ file.getName();
-									if (CB_Core_Settings.SpoilerFolderLocal.getValue().length() != 0)
+									ImageEntry image = cache.getSpoilerRessources().get(j);
+									String url = CB_Rpc_Settings.CBS_IP.getValue();
+									int pos = url.indexOf(":");
+									if (pos >= 0)
 									{
-										// Own Repo
-										imagePath = CB_Core_Settings.SpoilerFolderLocal.getValue() + "/"
-												+ cache.getGcCode().substring(0, 4) + "/" + file.getName();
+										url = url.substring(0, pos); // Port abschneiden, da dieser bereits in der imageURL der Images steht
 									}
-								}
-								else
-								{
-									imagePath = CB_Core_Settings.DescriptionImageFolder.getValue() + "/"
-											+ cache.getGcCode().substring(0, 4) + "/" + file.getName();
-									if (CB_Core_Settings.DescriptionImageFolderLocal.getValue().length() != 0)
+									url = "http://" + url + image.ImageUrl;
+									File file = new File(image.LocalPath);
+									url += cache.getGcCode().substring(0, 4) + "/";
+
+									url += file.getName().replace(" ", "%20");
+
+									String imagePath;
+									if (image.ImageUrl.indexOf("/spoilers/") >= 0)
 									{
-										// Own Repo
-										imagePath = CB_Core_Settings.DescriptionImageFolderLocal.getValue() + "/"
-												+ cache.getGcCode().substring(0, 4) + "/" + file.getName();
+										imagePath = CB_Core_Settings.SpoilerFolder.getValue() + "/" + cache.getGcCode().substring(0, 4)
+												+ "/" + file.getName();
+										if (CB_Core_Settings.SpoilerFolderLocal.getValue().length() != 0)
+										{
+											// Own Repo
+											imagePath = CB_Core_Settings.SpoilerFolderLocal.getValue() + "/"
+													+ cache.getGcCode().substring(0, 4) + "/" + file.getName();
+										}
 									}
-								}
-								file = new File(imagePath);
-								if (!file.exists())
-								{
-									DescriptionImageGrabber.Download(url, imagePath);
+									else
+									{
+										imagePath = CB_Core_Settings.DescriptionImageFolder.getValue() + "/"
+												+ cache.getGcCode().substring(0, 4) + "/" + file.getName();
+										if (CB_Core_Settings.DescriptionImageFolderLocal.getValue().length() != 0)
+										{
+											// Own Repo
+											imagePath = CB_Core_Settings.DescriptionImageFolderLocal.getValue() + "/"
+													+ cache.getGcCode().substring(0, 4) + "/" + file.getName();
+										}
+									}
+									file = new File(imagePath);
+									if (!file.exists())
+									{
+										DescriptionImageGrabber.Download(url, imagePath);
+									}
 								}
 							}
 						}
+						// Logs in DB eintragen. Die Logs sind hier nicht zu jedem Cache gespeichert sondern komplett in einer Liste
+						for (LogEntry log : gclAnswer.getLogs())
+						{
+							logDAO.WriteToDatabase(log);
+						}
+						startIndex += anzToLoad;
+						dataAvailable = gclAnswer.isDataAvailable(); // weitere Daten vorhanden?
+						System.gc();
 					}
-					// Logs in DB eintragen. Die Logs sind hier nicht zu jedem Cache gespeichert sondern komplett in einer Liste
-					for (LogEntry log : gclAnswer.getLogs())
+					else
 					{
-						logDAO.WriteToDatabase(log);
+						// Laden nicht erfolgreich
+						dataAvailable = false; // keine weiteren Daten laden!!!
 					}
-					startIndex += anzToLoad;
-					dataAvailable = gclAnswer.isDataAvailable(); // weitere Daten vorhanden?
-					System.gc();
-				}
-				else
-				{
-					// Laden nicht erfolgreich
-					dataAvailable = false; // keine weiteren Daten laden!!!
 				}
 			}
 		}
+		finally
+		{
+			Database.Data.endTransaction();
+		}
 		long endTS = System.currentTimeMillis();
-		System.out.println("Import Dauer: " + (endTS - startTS) + "ms");
+		System.out.println("Import Ende (" + (System.currentTimeMillis() - tmpTS) + ")");
+		tmpTS = System.currentTimeMillis();
+		System.out.println("Import Gesamtdauer: " + (endTS - startTS) + "ms");
 		// Aufzeichnen der Änderungen aktivieren
 		if (Database.Data.MasterDatabaseId == 0)
 		{
