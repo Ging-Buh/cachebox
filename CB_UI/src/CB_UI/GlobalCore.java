7ea13d38482dd28b81405c98ec94232f8b56ea89
longri <www.longri.de@googlemail.com>
2014-09-12 19:45:03 +0000

    increase Rev
    git-svn-id: svn://svn.code.sf.net/p/droidcachebox/code/trunk@2280 5890f383-b09f-485f-8f59-22bc967d67fd

--- CB_UI/src/CB_UI/GlobalCore.java
+++ CB_UI/src/CB_UI/GlobalCore.java
@@ -59,7 +59,7 @@ import CB_Utils.Log.Logger.iCreateDebugWithHeader;
  */
 public class GlobalCore extends CB_UI_Base.Global implements SolverCacheInterface
 {
-	public static final int CurrentRevision = 2279;
+	public static final int CurrentRevision = 2280;
 
 	public static final String CurrentVersion = "0.7.";
 	public static final String VersionPrefix = "test";
