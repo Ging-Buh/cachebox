3f9b0ca42e4c1ab1c5589d81e8a8b0d7db78b535
www.longri.de@googlemail.com <www.longri.de@googlemail.com>
2016-02-25 11:53:20 +0100

    + add HTML_Segment_Table and parse segments

---
+++ CB_UI_Base/src/CB_UI_Base/GL_UI/Controls/html/HTML_Segment_Table.java
@@ -0,0 +1,114 @@
+/* 
+ * Copyright (C) 2015 team-cachebox.de
+ *
+ * Licensed under the : GNU General Public License (GPL);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package CB_UI_Base.GL_UI.Controls.html;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import CB_UI_Base.Math.Stack;
+import CB_UI_Base.Math.UiSizes;
+import net.htmlparser.jericho.Attribute;
+import net.htmlparser.jericho.Attributes;
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.Tag;
+
+/**
+ * 
+ * @author Longri
+ *
+ */
+public class HTML_Segment_Table extends Html_Segment {
+
+    private final List<Html_Segment> listEntrys = new ArrayList<Html_Segment>();
+    private float scaledfontSize;
+    private final ArrayList<ArrayList<ArrayList<Html_Segment>>> tableSegments;
+
+    public HTML_Segment_Table(Stack<Tag> atributeStack, ArrayList<ArrayList<ArrayList<Html_Segment>>> tableSegments) {
+	super(Html_Segment_Typ.Table, atributeStack, "");
+	this.tableSegments = tableSegments;
+	resolveAtributes();
+    }
+
+    @Override
+    public void resolveAtributes() {
+	resolveHAlignment();
+
+	int size = 3;
+	for (Tag tag : tags) {
+	    if (!tag.getName().toLowerCase().equals("font"))
+		continue;
+	    List<Element> elements = tag.getAllElements();
+
+	    if (elements.isEmpty())
+		elements.add(tag.getElement());
+
+	    for (Element ele : elements) {
+		// tag.getElement().
+		Attributes attributes = ele.getAttributes();
+		for (Attribute attr : attributes) {
+		    if (attr.getKey().equals("size")) {
+
+			/*
+			 * The following values are acceptable:
+			 *
+			 * 1, 2, 3, 4, 5, 6, 7 +1, +2, +3, +4, +5, +6 -1, -2,
+			 * -3, -4, -5, -6
+			 */
+
+			String value = attr.getValue();
+			if (value.startsWith("+")) {
+			    int intSize = Integer.parseInt(value.replace("+", ""));
+			    size += intSize;
+			} else if (value.startsWith("-")) {
+			    int intSize = Integer.parseInt(value.replace("-", ""));
+			    size -= intSize;
+			} else {
+			    size = Integer.parseInt(value);
+			}
+
+		    }
+		}
+	    }
+	}
+	if (size < 1)
+	    size = 1;
+	if (size > 7)
+	    size = 7;
+
+	this.scaledfontSize = Html_Segment_TextBlock.getFontPx(size) * UiSizes.that.getScale()
+		* Html_Segment_TextBlock.DEFAULT_FONT_SIZE_FACTOR;
+
+    }
+
+    public float getFondSize() {
+	return this.scaledfontSize;
+    }
+
+    @Override
+    public void dispose() {
+	super.dispose();
+	if (listEntrys != null) {
+	    for (Html_Segment entry : listEntrys) {
+		if (entry != null) {
+		    entry.dispose();
+		    entry = null;
+		}
+	    }
+	    listEntrys.clear();
+	}
+    }
+
+}
--- CB_UI_Base/src/CB_UI_Base/GL_UI/Controls/html/elementhandler/TableElementHandler.java
+++ CB_UI_Base/src/CB_UI_Base/GL_UI/Controls/html/elementhandler/TableElementHandler.java
@@ -1,33 +1,56 @@
 package CB_UI_Base.GL_UI.Controls.html.elementhandler;
 
+import java.io.IOException;
+import java.util.List;
+
 import CB_UI_Base.GL_UI.Controls.html.CB_HtmlProcessor;
-import CB_UI_Base.GL_UI.Controls.html.CB_Html_Renderer;
 import net.htmlparser.jericho.Element;
 import net.htmlparser.jericho.Processor;
 import net.htmlparser.jericho.Renderer.AbstractBlockElementHandler;
 import net.htmlparser.jericho.Renderer.ElementHandler;
 
-import java.io.IOException;
-
 public final class TableElementHandler extends AbstractBlockElementHandler {
     public static final ElementHandler INSTANCE = new TableElementHandler(0, 0, false);
 
-
     private TableElementHandler(int topMargin, int bottomMargin, boolean indent) {
-        super(topMargin, bottomMargin, indent);
+	super(topMargin, bottomMargin, indent);
 
     }
 
     @Override
     protected void processBlockContent(Processor x, Element element) throws IOException {
 
-        System.out.println("new Table");
+	List<Element> rows = element.getAllElements("tr");
 
+	int colSize = 0;
+	for (Element row : rows) {
+
+	    List<Element> col = row.getAllElements("td");
+	    colSize = Math.max(colSize, col.size());
+	}
+
+	//
+
+	Element[][] tableElements = new Element[rows.size()][colSize];
+
+	// fill table content elements
+	int rowIdx = 0;
+	for (Element row : rows) {
+	    int colIdx = 0;
+	    for (Element col : row.getAllElements("td")) {
+		tableElements[rowIdx][colIdx++] = col;
+	    }
+	    rowIdx++;
+	}
+
+	((CB_HtmlProcessor) x).newTable(tableElements);
+
+	System.out.println("new Table");
 
     }
 
     @Override
     protected AbstractBlockElementHandler newInstance(int topMargin, int bottomMargin, boolean indent) {
-        return new TableElementHandler(topMargin, bottomMargin, indent);
+	return new TableElementHandler(topMargin, bottomMargin, indent);
     }
 }
