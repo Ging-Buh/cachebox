e60b5f1af76b8b6aeb0604a1be308831d1eb9e5c
www.longri.de@googlemail.com <www.longri.de@googlemail.com>
2016-03-31 08:33:59 +0200

    Version++

--- CB_UI/src/CB_UI/GlobalCore.java
+++ CB_UI/src/CB_UI/GlobalCore.java
@@ -56,10 +56,10 @@ import CB_Utils.Log.Log;
 public class GlobalCore extends CB_UI_Base.Global implements SolverCacheInterface {
 	final static org.slf4j.Logger log = LoggerFactory.getLogger(GlobalCore.class);
 
-	public static final int CurrentRevision = 20160330;
+	public static final int CurrentRevision = 20160331;
 
 	public static final String CurrentVersion = "0.8.";
-	public static final String VersionPrefix = "Test-svn2747";
+	public static final String VersionPrefix = "Test-svn2749";
 
 	public static final String aboutMsg1 = "Team Cachebox (2011-2016)" + br;
 	public static final String teamLink = "www.team-cachebox.de";
