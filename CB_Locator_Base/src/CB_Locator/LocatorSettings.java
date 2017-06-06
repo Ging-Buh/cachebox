0175b2efda285aced7f46654c3b0da8938139faf
Franz Frese <arbor95@gmx.de>
2017-06-06 09:52:34 +0200

    svn 30131: correct settings for additional maps

--- CB_Locator_Base/src/CB_Locator/LocatorSettings.java
+++ CB_Locator_Base/src/CB_Locator/LocatorSettings.java
@@ -60,13 +60,14 @@ public interface LocatorSettings {
 	public static final SettingFile MapsforgeNightTheme = (SettingFile) SettingsList.addSetting(new SettingFile("MapsforgeNightTheme", SettingCategory.Map, NEVER, "", SettingStoreType.Global, SettingUsage.ACB, "xml"));
 	public static final SettingFile MapsforgeCarDayTheme = (SettingFile) SettingsList.addSetting(new SettingFile("MapsforgeCarDayTheme", SettingCategory.Map, NEVER, "", SettingStoreType.Global, SettingUsage.ACB, "xml"));
 	public static final SettingFile MapsforgeCarNightTheme = (SettingFile) SettingsList.addSetting(new SettingFile("MapsforgeCarNightTheme", SettingCategory.Map, NEVER, "", SettingStoreType.Global, SettingUsage.ACB, "xml"));
-	public static final SettingString MapsforgeDayStyle = new SettingString("MapsforgeDayStyle", SettingCategory.Map, NEVER, "", SettingStoreType.Global, SettingUsage.ACB);
-	public static final SettingString MapsforgeNightStyle = new SettingString("MapsforgeNightStyle", SettingCategory.Map, NEVER, "", SettingStoreType.Global, SettingUsage.ACB);
-	public static final SettingString MapsforgeCarDayStyle = new SettingString("MapsforgeCarDayStyle", SettingCategory.Map, NEVER, "", SettingStoreType.Global, SettingUsage.ACB);
-	public static final SettingString MapsforgeCarNightStyle = new SettingString("MapsforgeCarNightStyle", SettingCategory.Map, NEVER, "", SettingStoreType.Global, SettingUsage.ACB);
+	public static final SettingString MapsforgeDayStyle = (SettingString) SettingsList.addSetting(new SettingString("MapsforgeDayStyle", SettingCategory.Map, NEVER, "", SettingStoreType.Global, SettingUsage.ACB));
+	public static final SettingString MapsforgeNightStyle = (SettingString) SettingsList.addSetting(new SettingString("MapsforgeNightStyle", SettingCategory.Map, NEVER, "", SettingStoreType.Global, SettingUsage.ACB));
+	public static final SettingString MapsforgeCarDayStyle = (SettingString) SettingsList.addSetting(new SettingString("MapsforgeCarDayStyle", SettingCategory.Map, NEVER, "", SettingStoreType.Global, SettingUsage.ACB));
+	public static final SettingString MapsforgeCarNightStyle = (SettingString) SettingsList.addSetting(new SettingString("MapsforgeCarNightStyle", SettingCategory.Map, NEVER, "", SettingStoreType.Global, SettingUsage.ACB));
 
-	public static final SettingString UserMap1 = new SettingString("UserMap1", SettingCategory.Map, EXPERT, "http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", SettingStoreType.Global, SettingUsage.ALL);
-	public static final SettingString UserMap2 = new SettingString("UserMap2", SettingCategory.Map, EXPERT, "", SettingStoreType.Global, SettingUsage.ALL);
+	public static final SettingString UserMap1 = (SettingString) SettingsList
+			.addSetting(new SettingString("UserMap1", SettingCategory.Map, EXPERT, "http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", SettingStoreType.Global, SettingUsage.ALL));
+	public static final SettingString UserMap2 = (SettingString) SettingsList.addSetting(new SettingString("UserMap2", SettingCategory.Map, EXPERT, "", SettingStoreType.Global, SettingUsage.ALL));
 
 	public static final SettingEnum<GL_RenderType> MapsforgeRenderType = new SettingEnum<GL_RenderType>("MapsforgeRenderType", SettingCategory.Map, EXPERT, GL_RenderType.Mapsforge, SettingStoreType.Global, SettingUsage.ACB, GL_RenderType.Mapsforge);
 
