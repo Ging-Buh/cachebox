81c95f7aca573b59801b65b5fc8861a1bb8e3536
Longri <develop@longri.de>
2019-01-08 12:12:16 +0100

    enable CB_Server Module fix compile error add TODO NEW API at ImportScheduler.java for uncomment lines

--- CB_Server/src/cb_server/CBS_Settings.java
+++ CB_Server/src/cb_server/CBS_Settings.java
@@ -6,13 +6,13 @@ public interface CBS_Settings {
 
 	// Abk�rzende Schreibweisen f�r die �bersichlichkeit bei den add Methoden
 	public static final SettingModus DEVELOPER = SettingModus.DEVELOPER;
-	public static final SettingModus NORMAL = SettingModus.Normal;
-	public static final SettingModus EXPERT = SettingModus.Expert;
-	public static final SettingModus NEVER = SettingModus.Never;
+	public static final SettingModus NORMAL = SettingModus.NORMAL;
+	public static final SettingModus EXPERT = SettingModus.EXPERT;
+	public static final SettingModus NEVER = SettingModus.NEVER;
 
-	public static final SettingInt Port = new SettingInt("Port", SettingCategory.Login, SettingModus.Normal, 7765, SettingStoreType.Global, SettingUsage.CBS);
-	public static final SettingInt PQImportInterval = new SettingInt("PQImportInterval (hours)", SettingCategory.API, SettingModus.Normal, 0, SettingStoreType.Global, SettingUsage.CBS);
-	public static final SettingString PQImportNames = new SettingString("PQImportNames", SettingCategory.API, SettingModus.Normal, "", SettingStoreType.Local, SettingUsage.CBS);
+	public static final SettingInt Port = new SettingInt("Port", SettingCategory.Login, NEVER, 7765, SettingStoreType.Global, SettingUsage.CBS);
+	public static final SettingInt PQImportInterval = new SettingInt("PQImportInterval (hours)", SettingCategory.API, NORMAL, 0, SettingStoreType.Global, SettingUsage.CBS);
+	public static final SettingString PQImportNames = new SettingString("PQImportNames", SettingCategory.API, NORMAL, "", SettingStoreType.Local, SettingUsage.CBS);
 	public static final SettingFile CBS_Mapsforge_Map = new SettingFile("CBS_Mapsforge_Map", SettingCategory.Map, NORMAL, "", SettingStoreType.Platform, SettingUsage.CBS, "map");
 
 }
